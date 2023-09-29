0ec8506c0030305e1d99b914244393d72be805f3
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-09-28 22:18:22 -0300

    feature: funçao media_temp e atualiza_temp

--- exercicio2parte2.cpp
+++ exercicio2parte2.cpp
@@ -3,26 +3,43 @@
 #include <ctime>
 
 float numero_aleatorio();
-float temp_maior(float vetor[]);
-float temp_menor(float vetor[]);
+float temp_maior(float vetor[], int tam);
+float temp_menor(float vetor[], int tam);
+float media_temp(float vetor[], int tam);
+float* atualiza_temp(float vetor[], int tam, float media);
 using namespace std;
 
 int main() {
-    float valor_digitado[250];
-    float maior_ , menor_;
+    float valor_digitado[10];
+    int tamanho_vetor = sizeof(valor_digitado) / sizeof(valor_digitado[0]);
+    float maior_ , menor_, media_;
+    float * vetor_atuazado; 
     // Inicializa a semente para a geração de números aleatórios
     srand(static_cast<unsigned>(time(nullptr)));
 
-    for (int i = 0; i < 250; i++) {
+    for (int i = 0; i < tamanho_vetor; i++) {
         valor_digitado[i] = numero_aleatorio();
     }
 
-    maior_ = temp_maior(valor_digitado);
-    menor_ = temp_menor(valor_digitado);
+    maior_ = temp_maior(valor_digitado, tamanho_vetor);
+    menor_ = temp_menor(valor_digitado, tamanho_vetor);
+    media_ = media_temp(valor_digitado, tamanho_vetor);
     cout <<"-----------------------------" <<
     "\nMaior temperatuda : "<< maior_ << 
     "\n-----------------------------" <<
-    "\nMenor temperatura : "<< menor_<<endl;
+    "\nMenor temperatura : "<< menor_<<
+    "\n-----------------------------" <<
+    "\nMedia : "<< media_ <<  endl;
+
+
+    vetor_atuazado = atualiza_temp(valor_digitado, tamanho_vetor, media_);
+
+    for (int i = 0; i < tamanho_vetor; i++) {
+        cout << "temperatura atualizada: " <<vetor_atuazado[i] << endl;
+    }
+
+    // Não se esqueça de liberar a memória alocada
+    delete[] vetor_atuazado;
 
     return 0;
 }
@@ -30,33 +47,57 @@ int main() {
 
 float numero_aleatorio() {
     float valor_random = 1 + 30.0 *(rand() %100) / 3000.0; 
-    cout << "Numero aleatorio: " << valor_random << endl;
+    cout << "temperatura: " << valor_random << endl;
     return valor_random;
 }
 
-float temp_maior(float vetor[]){
+float temp_maior(float vetor[],int tam){
 
     float maior = 0;
     maior = vetor[0];
-    for (int i = 0; i <=249; i++){       
+    for (int i = 0; i <tam; i++){       
         if(vetor[i] > maior){
             maior = vetor[i];
         }
     }
     return maior;
-
 }
 
-float temp_menor(float vetor[]){
+float temp_menor(float vetor[], int tam){
 
     float menor = 0;
     menor = vetor[0];
-    for (int i = 0; i <=249; i++){       
+    for (int i = 0; i <tam; i++){       
         if(vetor[i] < menor){
             menor = vetor[i];
         }
     }
-
-    return menor;
-    
+    return menor; 
 }
+
+float media_temp(float vetor[], int tam){
+    float soma = 0;
+    float media_cal = 0;
+    for (int i = 0; i <tam; i++){       
+        soma += vetor[i];
+    }
+    media_cal = soma / tam;
+
+    return media_cal;
+
+}    
+
+
+float* atualiza_temp(float vetor[], int tam, float media){
+
+    for (int i = 0; i <tam; i++){       
+        if(vetor[i] > media ){
+            vetor[i]++;
+        }else if(vetor[i] < media ){
+            vetor[i] --;
+            vetor[i] --;
+        }
+    }
+    return vetor;
+
+}  
